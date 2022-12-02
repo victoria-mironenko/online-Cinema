ce29cf094664a455e3bffe46b85314111f9fde6c
Vika Mironenko <mironenko-vika-2010@mail.ru>
2022-12-02 17:08:15 +0300

    one more

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { MOVIES } from "../MOCK/movies";
 
 class MovieService {
@@ -10,14 +9,3 @@ class MovieService {
 }
 
 export const movieService = new MovieService();
-=======
-import { MOVIES } from '../MOCK/movies'
-
-class MovieServise {
-     
-    getAllMovie() {
-        return Promise.resolve({ data: {} })
-    }
-}
-export const movieServise = new MovieServise();
->>>>>>> 8f93e5a94e17e26c32403583aa03c893b1cfc9f3
