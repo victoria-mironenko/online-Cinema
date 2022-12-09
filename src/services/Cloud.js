533c84665f32519922026f591bf9b5a4d4bd4497
Maksemqwe123 <Makcemjgffjh4gmail.com>
2022-12-09 18:46:10 +0300

    fix errors

--- src/services/Cloud.js
+++ src/services/Cloud.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 export class CloudService {
   constructor() {
     this.config = {
-      apiKey: "AIzaSyCHXodDUg3HOQr_44pZKfQZQpuMo-0LiUo",
+      apiKey: process.env.API_KEY,
       authDomain: "online-cinema-52ff2.firebaseapp.com",
       projectId: "online-cinema-52ff2",
       storageBucket: "online-cinema-52ff2.appspot.com",
       messagingSenderId: "284171025466",
-      appId: "1:284171025466:web:8451aff6d7c0f1b46c0903",
+      appId: process.env.API_ID,
       measurementId: "G-8H50WMGL3R",
     };
 
--- webpack.base.config.js
+++ webpack.base.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const Dotenv = required('dotenv-webpack');
+const Dotenv = require('dotenv-webpack');
 
 const PATHS = {
   src: path.resolve(__dirname, "./src"),
